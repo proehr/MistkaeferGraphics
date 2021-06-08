3c881a3e487c488267b4a209bf2c556b3f11c563
Silices <bar.tholomaeus@hotmail.de>
2021-06-08 22:44:40 +0200

    32-b-implement-parametric-surfaces
    - MeshGenerator.cs: generates the selected mesh
    - Generator prefab: holds MeshGenerator, a MeshCollider and a Rigidbody
    - Bartho Scene: contains 3 Generators and a Floor

---
+++ Assets/Scripts/MeshGenerator.cs
@@ -0,0 +1,141 @@
+using System;
+using System.CodeDom.Compiler;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ *  Author: Marting Zier - HTW Berlin 
+ *  Editor: Bartholomäus Berresheim
+ */
+public class MeshGenerator : MonoBehaviour
+{
+    private Mesh generatedMesh = null;
+    [SerializeField] private MeshFilter meshfilter = null;
+    [SerializeField] private MeshCollider meshcollider = null;
+    [Range (1,84)] [SerializeField] private int subdivisionSize = 25;
+    private enum Surfaces {Dini, Enneper, Torus}
+
+    private Vector2 uBounds;
+    private Vector2 vBounds;
+
+    [SerializeField] private Surfaces surfaceType;
+
+    // Dini, Enneper, torus By @Bartholomäus Berresheim
+    private void Start()
+    {
+        switch (surfaceType)
+        {
+            case Surfaces.Dini:
+                uBounds = new Vector2(0, (float) (Math.PI * 4));
+                vBounds = new Vector2( 0.001f, 2);
+                break;
+            case Surfaces.Enneper:
+                uBounds = new Vector2(0, 1.15f);
+                vBounds = new Vector2((float) (-Math.PI),(float) (Math.PI));
+                break;
+            case Surfaces.Torus:
+                uBounds = new Vector2(0.001f, (float) (Math.PI* 2));
+                vBounds = new Vector2(.001f,(float) (Math.PI* 2));
+                break;
+        }
+        Generate();
+    }
+    
+    // By @Martin Zier
+    void Generate()
+     {
+         generatedMesh = new Mesh();
+         meshfilter.mesh = generatedMesh;
+         
+         var subdivisions = new Vector2Int(subdivisionSize, subdivisionSize);     
+         var vertexSize = subdivisions + new Vector2Int(1, 1);            
+                                                                                        
+         var vertices = new Vector3[vertexSize.x * vertexSize.y];                     
+         var uvs = new Vector2[vertices.Length];                                     
+
+         for (var y = 0; y < vertexSize.y; y++)                            
+         {
+             var vNormalized = (1f / subdivisions.y) * y;                                
+             var v = Mathf.Lerp(vBounds.x, vBounds.y, vNormalized);                     // boundary check
+
+             for (var x = 0; x < vertexSize.x; x++)                                   
+             {
+                 var uNormalized = (1f / subdivisions.x) * x;                       
+                 var u = Mathf.Lerp(uBounds.x, uBounds.y, uNormalized);                  // boundary check  
+                 
+                 var vertex = CalculateVertex(u,v);                                
+                 
+                 var uv = new Vector2(u, v);                                       
+
+                 var arrayIndex = x + y * vertexSize.x;
+
+                 vertices[arrayIndex] = vertex;
+                 uvs[arrayIndex] = uv;
+             }
+
+             generatedMesh.vertices = vertices;
+             generatedMesh.uv = uvs;
+         }
+
+         var triangles = new int[subdivisions.x * subdivisions.y * 6];
+
+         for (var i = 0; i < subdivisions.x * subdivisions.y; i++)
+         {
+             var triangleIndex = (i % subdivisions.x) + (i / subdivisions.x) * vertexSize.x;
+
+             var indexer = i * 6;
+
+             triangles[indexer + 0] = triangleIndex;
+             triangles[indexer + 1] = triangleIndex + subdivisions.x + 1;
+             triangles[indexer + 2] = triangleIndex + 1;
+             
+             triangles[indexer + 3] = triangleIndex + 1;
+             triangles[indexer + 4] = triangleIndex + subdivisions.x + 1;
+             triangles[indexer + 5] = triangleIndex + subdivisions.x + 2;
+
+         }
+
+         generatedMesh.vertices = vertices;
+         generatedMesh.uv = uvs;
+         generatedMesh.triangles = triangles;
+
+         generatedMesh.RecalculateBounds();
+         generatedMesh.RecalculateNormals();
+         generatedMesh.RecalculateTangents();
+
+         meshfilter.mesh= generatedMesh;
+         meshcollider.sharedMesh = generatedMesh;
+         
+     }
+    
+    // Dini, Enneper, Torus By @Bartholomäus Berresheim
+    Vector3 CalculateVertex(float u, float v)
+    {
+        float x = u;
+        float y = v;
+        float z = 0;
+        switch (surfaceType)
+        {
+            case Surfaces.Dini:
+                x = (float) (Math.Cos(u) * Math.Sin(v));
+                y = (float) (Math.Sin(u) * Math.Sin(v));
+                z = (float) (Math.Cos(v) + Math.Log(Math.Tan(v / 2)) + 0.2 * u);
+                break;
+            case Surfaces.Enneper:
+                x = u * (1 - u * u / 3 + v * v) / 3;
+                y = v * (1 - v * v / 3 + u * u) / 3;;
+                z = (u * u - v * v) / 3;
+                break;
+            case Surfaces.Torus:
+                float R = 1f;
+                float r = 0.5f;
+                x = (float) (R * Math.Cos(u) + r * Math.Cos(v) * Math.Cos(u));
+                y = (float) (R * Math.Sin(u) + r * Math.Cos(v) * Math.Sin(u) );
+                z = (float) (r * Math.Sin(v));
+                break;
+        }
+
+        return new Vector3(x, y, z);
+    }
+}
